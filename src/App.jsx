11179665cb61c5e73b48c978abfa61440ea817d2
IM JAEYOUNG <118138003+ImJaePang@users.noreply.github.com>
2024-12-12 00:41:09 +0000

    초반 css 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,46 @@
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <body>
+      <div id="header">
+        <img src="./assets/investment-calculator-log.png"/>
+        <h1>React Investment Calculator!!</h1>
+      </div>
+      <div id="user-input">
+        <span className="input-group">
+          <label>INITIAL INVESTMENT</label>
+          <input type="text"/>
+
+        </span>
+        <span className="input-group">
+          <label>INITIAL INVESTMENT</label>
+          <input type="text"/>
+
+        </span>
+        <span className="input-group">
+          <label>INITIAL INVESTMENT</label>
+          <input type="text"/>
+
+        </span>
+        <span className="input-group">
+          <label>INITIAL INVESTMENT</label>
+          <input type="text"/>
+
+        </span>
+
+      </div>
+      <table id="result">
+        <thead>
+          <tr><td>abc</td></tr>
+        </thead>
+        <tbody>
+          <tr><td>def</td></tr>
+        </tbody>
+      </table>
+
+
+    </body>
+    
   )
 }
 
